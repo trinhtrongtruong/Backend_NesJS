daea43ed4420ae2dd7e6174dd1893a1880296bec
trinhtrongtruong <truong20216@gmail.com>
2024-03-20 10:14:25 +0700

    create token with JWT

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,19 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(private usersService: UsersService) {}
+    constructor(
+      private usersService: UsersService,
+      private jwtService: JwtService
+      ) {}
 
     //username and pass are 2 parameter Passport Library throw back (ném về) 
-    async validateUser(username: string, pass: string): Promise<any> {
-        const user = await this.usersService.findOneByUsername(username);
-        if(user){
-          const isValid = this.usersService.isValidPassword(pass, user.password)
-          if(isValid == true){
-            return user;
+      async validateUser(username: string, pass: string): Promise<any> {
+          const user = await this.usersService.findOneByUsername(username);
+          if(user){
+            const isValid = this.usersService.isValidPassword(pass, user.password)
+            if(isValid == true){
+              return user;
+            }
           }
+          return null;
         }
-        return null;
+    
+      async login(user: any) {
+        const payload = { username: user.email, sub: user._id };
+        return {
+          access_token: this.jwtService.sign(payload),
+        };
       }
 }
