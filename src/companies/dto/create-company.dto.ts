7e4e6981163dc7d8a9199f07527d9a1d894b552f
trinhtrongtruong <truong20216@gmail.com>
2024-03-22 10:49:23 +0700

    Generate modules Company

---
+++ src/companies/dto/create-company.dto.ts
@@ -0,0 +1,12 @@
+import { IsNotEmpty } from 'class-validator';
+//Data Tranfer Object (DTO) // class{}
+export class CreateCompanyDto {
+    @IsNotEmpty({ message: 'Tên không được để trống' })
+    name: string;
+    
+    @IsNotEmpty({ message: 'Địa chỉ không được để trống' })
+    address: string;
+
+    @IsNotEmpty({ message: 'Mô tả không được để trống' })
+    description: string;
+}
