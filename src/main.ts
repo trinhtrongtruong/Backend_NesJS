58f4f0cffd9a7786ea8bf0a333a108ee45ab69b6
trinhtrongtruong <truong20216@gmail.com>
2024-03-23 18:03:52 +0700

    Transform Response

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config'; 
 import { ValidationPipe } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { TransformInterceptor } from './core/transform.interceptor';
 // require('dotenv').config();
 
 async function bootstrap() {
@@ -16,6 +17,8 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   const reflector = app.get( Reflector );
   app.useGlobalGuards( new JwtAuthGuard( reflector ) );
+  app.useGlobalInterceptors(new TransformInterceptor());
+
 
   // config cors
   app.enableCors(
